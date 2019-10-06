06c6f81ab43ca347238f64b69c17682ab53b0229
Chandler Giusti <ChandlerG@users.noreply.github.com>
2019-10-05 18:29:34 -0700

    Updates

---
+++ precache-manifest.d728b424f30752fe7c5642efaa7a5b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b11ef8a1ee54b0b1c49",
+    "url": "/chandy-dev-site/static/css/main.39a9fd32.chunk.css"
+  },
+  {
+    "revision": "6b11ef8a1ee54b0b1c49",
+    "url": "/chandy-dev-site/static/js/main.bf0c0f84.chunk.js"
+  },
+  {
+    "revision": "9f077899ca6cca3deaee",
+    "url": "/chandy-dev-site/static/js/runtime~main.5c145737.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/chandy-dev-site/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chandy-dev-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ba91a028d54fed8374731c0d3fe583c",
+    "url": "/chandy-dev-site/index.html"
+  }
+];
