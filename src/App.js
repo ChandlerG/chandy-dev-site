3e1a21748c5acea02925132e75a1b33840ecb17d
Chandler Giusti <ChandlerG@users.noreply.github.com>
2019-10-06 00:13:44 -0700

    using hashrouter i guess

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {HashRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 import './App.css';
 import {GoRef} from "./refcomponents/GoRef";
