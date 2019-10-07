2b1d8e95878575873fdadfe17328d2ad59bcb118
Christopher Hunter <hunter.christopher@icloud.com>
2019-10-07 13:23:46 -0700

    improve documentation and fix more syntax errors
    - I did not run this and just did it off the top of my head. Consider keeping these in source files and adding them to your executable when building the app. This would make it easier to test them (at least manually).

--- src/refcomponents/GoRef.jsx
+++ src/refcomponents/GoRef.jsx
@@ -10,29 +10,47 @@ package chandy
 
 import "strings"
 
+// Cat is used to map a Cat's name to their color
+// the Name should be unique within the program but
+// two Cats can have the same color descriptor
 type Cat struct {
 	Name, Color string
 }
 
-func PublicCatFunc() Cat {
-	dagwood = Cat{"Dagwood", "Tabby I Guess"}
-	sully = Cat{Name: "Sully", Color: "Tabby I Guess"}
+// CatColorDescriptorTabby is how I describe my Cat's tabby-ish color
+const CatColorDescriptorTabby = "Tabby I Guess"
+
+// PublicCatFunc returns a slice of special Cats
+func PublicCatFunc() []Cat {
+	dagwood := Cat{"Dagwood", CatColorDescriptorTabby}          // without field names (see order)
+	sully := Cat{Name: "Sully", Color: CatColorDescriptorTabby} // with field names (recomended)
+	return []Cat{dogwood, sully}
 }
 
-func privateArraysFunc() {
+// privateArraysFunc returns a slice of cat names
+func privateArraysFunc() []string {
 	catNames := [2]string{
 		"Dagwood",
 		"Sully",
 	}
-	arr := make([]int, length, capacity)
+	arr := make([]int, len(catNames), len(catNames))
+	for index, name := range catNames {
+		arr[index] = name
+	}
+	// could also be done as follows, this creates a slice for the given array. This would
+	// allow you to remove 36-39.
+	//   return catNames[:]
+	return arr
 }
 
+// privateMapsFunc returns a map of fields in s and the number of times they occur in s
 func privateMapsFunc(s string) map[string]int {
 	m := make(map[string]int)
-	sArr := strings.Split(s, " ")
+	sArr := strings.Split(s, " ") // if splitting by user provided whitespace, you may want to use [strings.Fields](https://golang.org/pkg/strings/#Fields)
 	for index, elem := range sArr {
-		m[s] = m[s] + 1
+		m[s] = m[s] + 1 // m[s]++ is not permitted so first get the value then set it.
 	}
+	return m
 }
            `} </pre>
         </div>
